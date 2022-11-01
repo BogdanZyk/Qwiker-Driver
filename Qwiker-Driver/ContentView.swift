dd28333559a6fafa9de8909014be9efbe73bb94e
Bogdan <zykovbogdan07@gmail.com>
2022-11-01 11:03:03 +0300

    Initial Commit

---
+++ Qwiker-Driver/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Qwiker-Driver
+//
+//  Created by Богдан Зыков on 01.11.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Qwiker-Driver/Qwiker_DriverApp.swift
@@ -0,0 +1,17 @@
+//
+//  Qwiker_DriverApp.swift
+//  Qwiker-Driver
+//
+//  Created by Богдан Зыков on 01.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct Qwiker_DriverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
